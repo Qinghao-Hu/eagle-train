e488f166613acd94390c1165e2d6a81283b61aa1
ys-2020 <shangy@nvidia.com>
2025-06-18 21:44:25 -0700

    [Minor] code sync

--- slurm/launch_train.sh
+++ slurm/launch_train.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #SBATCH -A nvr_elm_llm                  #account
-#SBATCH -p polar,grizzly,batch_block1,batch_block3,batch_block2,batch_block4,interactive                         #partition
+#SBATCH -p polar,grizzly,batch_block1,batch_block3,batch_block2,batch_block4                         #partition
 #SBATCH -t 04:00:00                     #wall time limit, hr:min:sec
-#SBATCH -N 3                            #number of nodes
+#SBATCH -N 4                           #number of nodes
 #SBATCH --mem=0                         # all mem avail
 #SBATCH --gres=gpu:8
 #SBATCH -J nvr_elm_llm-tlt-eagle        #job name
-#SBATCH --array=1-2%1
+#SBATCH --array=1-1%1
 #SBATCH --exclusive #important: need to make it exclusive to improve speed
 
 
@@ -22,7 +22,7 @@ HNAME=$(echo $(scontrol show hostname))
 
 echo "[$HNAME] | $PROGRESS | $WORKDIR | Running Jobs"
 
-cmd="/home/shangy/TLT/eagle-train/scripts/launch_by_slurm/datagen.sh /home/shangy/TLT/models/Qwen/Qwen2.5-7B-Instruct /home/shangy/TLT/dataset"
+cmd="/home/shangy/TLT/eagle-train/scripts/launch_by_slurm/datagen.sh /home/shangy/TLT/models/llama/Llama-3.1-8B-Instruct /home/shangy/TLT/dataset"
 
 srun \
     -o $OUTFILE -e $ERRFILE \
